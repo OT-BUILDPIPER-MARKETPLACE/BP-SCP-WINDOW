abf2cc624e70bfaada0d676ff1d5e5c2d0f1784c
Aayush Verma <aayush.verma@opstree.com>
2026-03-12 16:08:30 +0530

    add build.sh

--- Dockerfile
+++ Dockerfile
@@ -1,13 +1,26 @@
-FROM alpine
-RUN apk add --no-cache --upgrade bash
-RUN apk add jq
-COPY build.sh .
+FROM ubuntu:22.04
 
-ADD BP-BASE-SHELL-STEPS /opt/buildpiper/shell-functions/
+RUN apt update && apt install -y \
+  sshpass \
+  bash \
+  jq \
+  file \
+  dos2unix \
+  git
+
+RUN groupadd -g 65522 buildpiper && \
+    useradd -u 65522 -g buildpiper -d /home/buildpiper -m -s /bin/bash buildpiper && \
+    mkdir -p /bp /bp/workspace /bp/data /home/buildpiper/reports && \
+    chown -R buildpiper:buildpiper /bp /home/buildpiper
+
+COPY --chown=buildpiper:buildpiper build.sh /home/buildpiper/build.sh
+COPY --chown=buildpiper:buildpiper BP-BASE-SHELL-STEPS/ /opt/buildpiper/shell-functions/
 
 
 ENV SLEEP_DURATION 5s
-ENV ACTIVITY_SUB_TASK_CODE REPLACE_IT
-ENV VALIDATION_FAILURE_ACTION WARNING
+ENV ACTIVITY_SUB_TASK_CODE CF_STEP 
+
+USER buildpiper
+WORKDIR /home/buildpiper
 
 ENTRYPOINT [ "./build.sh" ]
--- build.sh
+++ build.sh
@@ -1,24 +1,49 @@
 #!/bin/bash
 source /opt/buildpiper/shell-functions/functions.sh
 source /opt/buildpiper/shell-functions/log-functions.sh
-source /opt/buildpiper/shell-functions/str-functions.sh
-source /opt/buildpiper/shell-functions/file-functions.sh
-source /opt/buildpiper/shell-functions/aws-functions.sh
-
-TASK_STATUS=0
-
+source /opt/buildpiper/shell-functions/git-functions.sh
+# Optionally enable debugging
+if [ "$DEBUG" = true ]; then
+  set -x  
+fi
+# Configuration
+WINDOWS_USER="$WINDOWS_USER"
+WINDOWS_IP="$WINDOWS_IP"
+WINDOWS_PASSWORD="$WINDOWS_PASSWORD"
+CODEBASE_DIR="$CODEBASE_DIR"                    
+Deploy_Environment=$DEPLOY_ENV
+#ARTIFACTS_PATH_LOCAL="$ARTIFACTS_PATH_LOCAL"
+ARTIFACTS_PATH_WIND=$ARTIFACTS_PATH_WIND
+#SHARED_LOCATION=$SHARED_LOCATION
 CODEBASE_LOCATION="${WORKSPACE}"/"${CODEBASE_DIR}"
 logInfoMessage "I'll do processing at [$CODEBASE_LOCATION]"
-sleep  $SLEEP_DURATION
-cd  "${CODEBASE_LOCATION}"
+cd "${CODEBASE_LOCATION}"
 
-TASK_STATUS=0
+# #Rename the artifact name
+# mv "$ARTIFACTS_PATH_LOCAL/${tag}-Resource Center-win32-x64.exe" "$ARTIFACTS_PATH_LOCAL/Resource Center-win.exe"
+# mkdir -p $ARTIFACTS_PATH_LOCAL/$DEPLOY_ENV/${tag}-PENDING_SANITY/
+# cp "$ARTIFACTS_PATH_LOCAL/Resource Center-win.exe" "$ARTIFACTS_PATH_LOCAL/$DEPLOY_ENV/${tag}-PENDING_SANITY/"
+# LOCAL_COPIED_ARTIFACT="$ARTIFACTS_PATH_LOCAL/$DEPLOY_ENV" 
+# WINDOWS_COPIED_ARTIFACT="$DEPLOY_ENV/${tag}-PENDING_SANITY/Resource Center-win.exe"
 
-if [condition]; then
-    logErrorMessage "Done the required operation"
+
+#Copying $CODEBASE_DIR
+logInfoMessage "I have started copying the artifact to [$ARTIFACTS_PATH_WIND]"
+sshpass -p "$WINDOWS_PASSWORD" scp -r -o StrictHostKeyChecking=no "$LOCAL_COPIED_ARTIFACT" "$WINDOWS_USER@$WINDOWS_IP:$ARTIFACTS_PATH_WIND\\"
+if [ $? -ne 0 ]; then
+  logErrorMessage "Failed to transfer Artifact on Windows VM."
+  exit 1
 else
-    TASK_STATUS=1
-    logErrorMessage "Target server not provided please check"
-
+  logInfoMessage "artifact copied successfully to Artifact on Windows VM"
 fi
+#Copying Artifact to shared location
+# sshpass -p "$WINDOWS_PASSWORD" ssh -v -o StrictHostKeyChecking=no -o ConnectTimeout=60 "$WINDOWS_USER@$WINDOWS_IP" \
+# "echo f | xcopy \"$ARTIFACTS_PATH_WIND\\$DEPLOY_ENV\\\" \"${SHARED_LOCATION}\\$DEPLOY_ENV\\\" /Y /R /I /E"
+# if [ $? -ne 0 ]; then
+#   logErrorMessage "Failed to transfer Artifact code to Shared location."
+#   exit 1
+# else
+#   logInfoMessage "artifact copied successfully to Artifact on Shared location"
+# fi
+TASK_STATUS=$?
 saveTaskStatus ${TASK_STATUS} ${ACTIVITY_SUB_TASK_CODE}
