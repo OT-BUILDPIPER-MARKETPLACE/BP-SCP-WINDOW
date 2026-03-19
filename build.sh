30ba098de027bbb3d904ae1b135f2679fe83f933
Aayush Verma <aayush.verma@opstree.com>
2026-03-19 11:36:19 +0530

    Update build.sh

--- build.sh
+++ build.sh
@@ -3,16 +3,15 @@ source /opt/buildpiper/shell-functions/functions.sh
 source /opt/buildpiper/shell-functions/log-functions.sh
 source /opt/buildpiper/shell-functions/git-functions.sh
 
-# Optionally enable debugging
 if [ "$DEBUG" = true ]; then
-  set -x  
+  set -x
 fi
-# Configuration
+
 WINDOWS_USER="$WINDOWS_USER"
 WINDOWS_IP="$WINDOWS_IP"
 WINDOWS_PASSWORD="$WINDOWS_PASSWORD"
-CODEBASE_DIR="$CODEBASE_DIR"                    
-CodebaseS_PATH_LOCAL="$CodebaseS_PATH_LOCAL"
+CODEBASE_DIR="$CODEBASE_DIR"
+ARTIFACTS_PATH="$ARTIFACTS_PATH"
 
 ENV="$ENV"
 CODEBASE_LOCATION="${WORKSPACE}"/"${CODEBASE_DIR}"
@@ -21,7 +20,7 @@ sleep $SLEEP_DURATION
 logInfoMessage "I'll do processing at [$CODEBASE_LOCATION]"
 cd "${CODEBASE_LOCATION}"
 
-COMPLETED_PATH="$CodebaseS_PATH_LOCAL/$ENV" 
+COMPLETED_PATH="$ARTIFACTS_PATH/$ENV"
 logInfoMessage "Complete path to Codebases: [$COMPLETED_PATH]"
 
 WINDOWS_PATH=$(echo "$COMPLETED_PATH" | sed 's#\\#/#g')
@@ -29,7 +28,14 @@ WINDOWS_PATH=$(echo "$COMPLETED_PATH" | sed 's#\\#/#g')
 logInfoMessage "Windows IP: $WINDOWS_IP"
 logInfoMessage "Windows path: $WINDOWS_PATH"
 
-# Clean directory only if flag is true
+
+if [ -n "$LOCAL_COPIED_ARTIFACT" ] && [ -e "$LOCAL_COPIED_ARTIFACT" ]; then
+    logInfoMessage "LOCAL_COPIED_ARTIFACT exists: $LOCAL_COPIED_ARTIFACT"
+else
+    logErrorMessage "LOCAL_COPIED_ARTIFACT not found or not provided"
+    exit 1
+fi
+
 if [ "$CLEAN_OLD_DIR" = "true" ]; then
   sshpass -p "$WINDOWS_PASSWORD" ssh -o StrictHostKeyChecking=no "$WINDOWS_USER@$WINDOWS_IP" \
   "powershell -NoProfile -ExecutionPolicy Bypass -Command \"if (Test-Path '$WINDOWS_PATH') { Remove-Item -Recurse -Force '$WINDOWS_PATH' -ErrorAction SilentlyContinue }; New-Item -ItemType Directory -Force -Path '$WINDOWS_PATH'\""
@@ -41,7 +47,7 @@ else
 fi
 
 logInfoMessage "I have started copying the Codebase to [$WINDOWS_PATH]"
-sshpass -p "$WINDOWS_PASSWORD" scp -r -o StrictHostKeyChecking=no "$LOCAL_COPIED_Codebase" "$WINDOWS_USER@$WINDOWS_IP:$WINDOWS_PATH\\"
+sshpass -p "$WINDOWS_PASSWORD" scp -r -o StrictHostKeyChecking=no "$LOCAL_COPIED_ARTIFACT" "$WINDOWS_USER@$WINDOWS_IP:$WINDOWS_PATH\\"
 logInfoMessage "Codebase copy completed to [$WINDOWS_PATH]"
 
 TASK_STATUS=$?
