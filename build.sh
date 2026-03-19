1e043dff9b734c82a5e1d0470404c993eb61469a
Aayush Verma <aayush.verma@opstree.com>
2026-03-19 09:54:14 +0530

    Update build.sh

--- build.sh
+++ build.sh
@@ -2,6 +2,7 @@
 source /opt/buildpiper/shell-functions/functions.sh
 source /opt/buildpiper/shell-functions/log-functions.sh
 source /opt/buildpiper/shell-functions/git-functions.sh
+
 # Optionally enable debugging
 if [ "$DEBUG" = true ]; then
   set -x  
@@ -11,39 +12,37 @@ WINDOWS_USER="$WINDOWS_USER"
 WINDOWS_IP="$WINDOWS_IP"
 WINDOWS_PASSWORD="$WINDOWS_PASSWORD"
 CODEBASE_DIR="$CODEBASE_DIR"                    
-Deploy_Environment=$DEPLOY_ENV
-#ARTIFACTS_PATH_LOCAL="$ARTIFACTS_PATH_LOCAL"
-ARTIFACTS_PATH_WIND=$ARTIFACTS_PATH_WIND
-#SHARED_LOCATION=$SHARED_LOCATION
+CodebaseS_PATH_LOCAL="$CodebaseS_PATH_LOCAL"
+
+ENV="$ENV"
 CODEBASE_LOCATION="${WORKSPACE}"/"${CODEBASE_DIR}"
+
+sleep $SLEEP_DURATION
 logInfoMessage "I'll do processing at [$CODEBASE_LOCATION]"
 cd "${CODEBASE_LOCATION}"
 
-# #Rename the artifact name
-# mv "$ARTIFACTS_PATH_LOCAL/${tag}-Resource Center-win32-x64.exe" "$ARTIFACTS_PATH_LOCAL/Resource Center-win.exe"
-# mkdir -p $ARTIFACTS_PATH_LOCAL/$DEPLOY_ENV/${tag}-PENDING_SANITY/
-# cp "$ARTIFACTS_PATH_LOCAL/Resource Center-win.exe" "$ARTIFACTS_PATH_LOCAL/$DEPLOY_ENV/${tag}-PENDING_SANITY/"
-# LOCAL_COPIED_ARTIFACT="$ARTIFACTS_PATH_LOCAL/$DEPLOY_ENV" 
-# WINDOWS_COPIED_ARTIFACT="$DEPLOY_ENV/${tag}-PENDING_SANITY/Resource Center-win.exe"
+COMPLETED_PATH="$CodebaseS_PATH_LOCAL/$ENV" 
+logInfoMessage "Complete path to Codebases: [$COMPLETED_PATH]"
 
+WINDOWS_PATH=$(echo "$COMPLETED_PATH" | sed 's#\\#/#g')
 
-#Copying $CODEBASE_DIR
-logInfoMessage "I have started copying the artifact to [$ARTIFACTS_PATH_WIND]"
-sshpass -p "$WINDOWS_PASSWORD" scp -r -o StrictHostKeyChecking=no "$LOCAL_COPIED_ARTIFACT" "$WINDOWS_USER@$WINDOWS_IP:$ARTIFACTS_PATH_WIND\\"
-if [ $? -ne 0 ]; then
-  logErrorMessage "Failed to transfer Artifact on Windows VM."
-  exit 1
+logInfoMessage "Windows IP: $WINDOWS_IP"
+logInfoMessage "Windows path: $WINDOWS_PATH"
+
+# Clean directory only if flag is true
+if [ "$CLEAN_OLD_DIR" = "true" ]; then
+  sshpass -p "$WINDOWS_PASSWORD" ssh -o StrictHostKeyChecking=no "$WINDOWS_USER@$WINDOWS_IP" \
+  "powershell -NoProfile -ExecutionPolicy Bypass -Command \"if (Test-Path '$WINDOWS_PATH') { Remove-Item -Recurse -Force '$WINDOWS_PATH' -ErrorAction SilentlyContinue }; New-Item -ItemType Directory -Force -Path '$WINDOWS_PATH'\""
+  logInfoMessage "Cleaned and created directory [$WINDOWS_PATH]"
 else
-  logInfoMessage "artifact copied successfully to Artifact on Windows VM"
+  sshpass -p "$WINDOWS_PASSWORD" ssh -o StrictHostKeyChecking=no "$WINDOWS_USER@$WINDOWS_IP" \
+  "powershell -NoProfile -ExecutionPolicy Bypass -Command \"New-Item -ItemType Directory -Force -Path '$WINDOWS_PATH'\""
+  logInfoMessage "Created directory or already exists [$WINDOWS_PATH]"
 fi
-#Copying Artifact to shared location
-# sshpass -p "$WINDOWS_PASSWORD" ssh -v -o StrictHostKeyChecking=no -o ConnectTimeout=60 "$WINDOWS_USER@$WINDOWS_IP" \
-# "echo f | xcopy \"$ARTIFACTS_PATH_WIND\\$DEPLOY_ENV\\\" \"${SHARED_LOCATION}\\$DEPLOY_ENV\\\" /Y /R /I /E"
-# if [ $? -ne 0 ]; then
-#   logErrorMessage "Failed to transfer Artifact code to Shared location."
-#   exit 1
-# else
-#   logInfoMessage "artifact copied successfully to Artifact on Shared location"
-# fi
+
+logInfoMessage "I have started copying the Codebase to [$WINDOWS_PATH]"
+sshpass -p "$WINDOWS_PASSWORD" scp -r -o StrictHostKeyChecking=no "$LOCAL_COPIED_Codebase" "$WINDOWS_USER@$WINDOWS_IP:$WINDOWS_PATH\\"
+logInfoMessage "Codebase copy completed to [$WINDOWS_PATH]"
+
 TASK_STATUS=$?
 saveTaskStatus ${TASK_STATUS} ${ACTIVITY_SUB_TASK_CODE}
